f00f039116cae536723da20a91fb0c94e43cf6a4
Xavier Gomez Ponce <xgp3@msn.com>
2020-11-25 11:01:53 +0100

    calendar dates

--- src/index.js
+++ src/index.js
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
+        var valueToPrint = null;
+        if (this.state.value == null) {
+            valueToPrint = this.props.value;
+        }
+        else {
+            valueToPrint =  this.state.value;
+        }
+
       return (
-        <button className="square">
-          {/* TODO */}
+        <button
+        className="square"
+        onClick = {() => {
+            var today = new Date();
+            var date = today.getDate();
+            if(date >= this.props.value) {
+                this.setState({value: 'X'})
+            }
+            else{
+                alert("You can't access yet!")
+            }
+            }
+            }>
+            {valueToPrint}
         </button>
       );
     }
@@ -14,29 +41,51 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i}/>;
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'El mejor calendario de adviento!';
   
       return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
+            {this.renderSquare()}          
             {this.renderSquare(1)}
             {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
             {this.renderSquare(3)}
             {this.renderSquare(4)}
             {this.renderSquare(5)}
+            {this.renderSquare(6)}
+
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
             {this.renderSquare(7)}
             {this.renderSquare(8)}
+            {this.renderSquare(9)}
+            {this.renderSquare(10)}
+            {this.renderSquare(11)}
+            {this.renderSquare(12)}
+            {this.renderSquare(13)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(14)}
+            {this.renderSquare(15)}
+            {this.renderSquare(16)}
+            {this.renderSquare(17)}
+            {this.renderSquare(18)}
+            {this.renderSquare(19)}
+            {this.renderSquare(20)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(21)}
+            {this.renderSquare(22)}
+            {this.renderSquare(23)}
+            {this.renderSquare(24)}
+            {this.renderSquare()}
+            {this.renderSquare()}
+            {this.renderSquare(27)}
           </div>
         </div>
       );
